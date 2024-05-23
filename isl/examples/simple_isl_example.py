2242b571cf4752280598df6e0df1ebaf12dfeb6f
Ben Jaderberg <14994219+bjader@users.noreply.github.com>
2024-05-23 11:00:57 +0100

    Revert assumed accidental changes to examples.

--- isl/examples/simple_isl_example.py
+++ isl/examples/simple_isl_example.py
@@ -1,7 +1,7 @@
 import logging
-import numpy as np
+
 import isl.utils.circuit_operations as co
-from isl.recompilers import ISLRecompiler, ISLConfig
+from isl.recompilers import ISLRecompiler
 
 logging.basicConfig()
 logger = logging.getLogger('isl')
@@ -10,18 +10,16 @@ logger.setLevel(logging.INFO)
 # Create circuit creating a random initial state
 qc = co.create_random_initial_state_circuit(4)
 
-config = ISLConfig(rotosolve_frequency=5, max_layers_to_modify=5)
-isl_recompiler = ISLRecompiler(qc, isl_config=config, backend=co.MPS_SIM)
+isl_recompiler = ISLRecompiler(qc)
 
-# Recompile, save recompiler object every 4 layers
-result = isl_recompiler.recompile(save_frequency=4)
-
-# Load recompiler after layer 4 and recompile from there
-recompiler_4 = np.load("recompiler_after_layer_4.npy", allow_pickle=True)[0]
-result_4 = recompiler_4.recompile()
-
-
-# Comparison
-print(result.time_taken)
-print(result_4.time_taken)
-print(co.calculate_overlap_between_circuits(result.circuit, result_4.circuit))
+result = isl_recompiler.recompile()
+approx_circuit = result["circuit"]
+print(f"Overlap between circuits is {result['overlap']}")
+print(f'{"-" * 32}')
+print(f'{"-" * 10}OLD  CIRCUIT{"-" * 10}')
+print(f'{"-" * 32}')
+print(qc)
+print(f'{"-" * 32}')
+print(f'{"-" * 10}ISL  CIRCUIT{"-" * 10}')
+print(f'{"-" * 32}')
+print(approx_circuit)
